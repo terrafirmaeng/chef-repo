6b16f3d244541916ad775c1362c75277a257dd2f
lane_maxwell <lane_maxwell@terrafirmaeng.com>
2014-11-20 11:51:14 -0600

    Added remote file pull and apache site

--- cookbooks/aar/recipes/default.rb
+++ cookbooks/aar/recipes/default.rb
@@ -1,8 +1,30 @@
-#
-# Cookbook Name:: aar
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+
+include_recipe "apache"
+include_recipe "mysql::server"
+include_recipe "unzip"
+
+
+
+
+# get the external file
+remote_file "#{Chef::Config[:file_cache_path]}/somefile.zip" do
+  source 'https://github.com/colincam/Awesome-Appliance-Repair/archive/master.zip'
+end
+
+
+execute "unzip somefile.zip" do
+  cwd Chef::Config[:file_cache_path]
+  creates "Awesome-Appliance-Repair-master"
+end
+
+execute "cp -R AAR /var/www" do
+  cwd "#{Chef::Config[:file_cache_path]}/Awesome-Appliance-Repair-master"
+  creates "/var/www/AAR"
+end
+
+execute "chown -R apache:apache AAR" do
+  cwd "/var/www"
+end
+
+

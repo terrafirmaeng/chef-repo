d1bd59c1f0b358ceb97dbffddbe458d84ec80a85
lane_maxwell <lane_maxwell@terrafirmaeng.com>
2014-11-20 13:34:59 -0600

    Adding configuration

--- cookbooks/aar/recipes/apache-server.rb
+++ cookbooks/aar/recipes/apache-server.rb
@@ -3,3 +3,8 @@ template "/etc/httpd/conf.d/aar.conf" do
   mode '0644'
   notifies :restart, 'service[httpd]'
 end
+
+template "/var/www/AAR/AAR_config.py" do
+  source 'AAR_Config.py.erb'
+  mode '0644'
+end
